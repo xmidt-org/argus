c836bd588d567ff6319e25e3c1355fb0a668a9fc
Joel Unzain <joel_unzain@comcast.com>
2021-02-03 18:07:28 -0800

    Add comments to exported param structs

--- chrysom/parameterStructs.go
+++ chrysom/parameterStructs.go
@@ -2,7 +2,7 @@ package chrysom
 
 import "github.com/xmidt-org/argus/model"
 
-// GetItemsInput contains all the needed parameters for GetItems
+// GetItemsInput contains input parameters for GetItems.
 type GetItemsInput struct {
 	// Bucket is the name of the bucket from which to fetch the items.
 	Bucket string
@@ -12,29 +12,44 @@ type GetItemsInput struct {
 	Owner string
 }
 
-// GetItemsOutput contains all the output parameters for GetItems.
+// GetItemsOutput output parameters for GetItems.
 // Note: errors are reported separately.
 type GetItemsOutput struct {
 	Items []model.Item
 }
 
+// PushItemInput contains input parameters for PushItem.
 type PushItemInput struct {
-	ID     string
+	// ID is the unique identifier for the item within the given bucket.
+	ID string
+
+	// Bucket is the name of the item grouping the item to be deleted belongs to.
 	Bucket string
-	Owner  string
-	Item   model.Item
+
+	// Owner is the name of the owner associated to this item.
+	// (Optional)
+	Owner string
+
+	// Item contains the item data to be pushed.
+	Item model.Item
 }
 
+// PushItemOput contains output parameters for PushItem.
 type PushItemOutput struct {
+	// Result reports whether the successful item push operation was
+	// an update or a creation.
 	Result PushResult
 }
 
+// RemoveItemInput contains input parameters for RemoveItem.
 type RemoveItemInput struct {
 	ID     string
 	Bucket string
 	Owner  string
 }
 
+// RemoveItemOutput contains output parameters for RemoveItem.
 type RemoveItemOutput struct {
+	// Item is the data of the item which was deleted.
 	Item model.Item
 }
