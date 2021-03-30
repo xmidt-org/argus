ec9d9911636507cc47a4b9917e7c2ce06162de70
Joel Unzain <joel_unzain@comcast.com>
2021-03-29 17:50:06 -0700

    new GetAll test and add comments to rm tests

--- store/dynamodb/service_test.go
+++ store/dynamodb/service_test.go
@@ -117,7 +117,7 @@ func testClientErrors(t *testing.T) {
 	suite.Run(t, new(ClientErrorTestSuite))
 }
 
-func genTestPutItemInput(key model.Key, item store.OwnableItem) *dynamodb.PutItemInput {
+func getTestPutItemInput(key model.Key, item store.OwnableItem) *dynamodb.PutItemInput {
 	storingItem := storableItem{
 		OwnableItem: item,
 		Key:         key,
@@ -212,7 +212,7 @@ func TestNewPushItem(t *testing.T) {
 				putItemOutput, putItemErr = nil, dbErr
 			}
 
-			m.On("PutItem", genTestPutItemInput(tc.Key, tc.Item)).Return(putItemOutput, putItemErr)
+			m.On("PutItem", getTestPutItemInput(tc.Key, tc.Item)).Return(putItemOutput, putItemErr)
 			cc, err := sv.Push(tc.Key, tc.Item)
 			assert.Equal(tc.ExpectedConsumedCapacity, cc)
 			assert.Equal(tc.ExpectedError, err)
@@ -220,14 +220,27 @@ func TestNewPushItem(t *testing.T) {
 		})
 
 	}
-	//  Cases
-	// (2) Include TTL, dynamo client fails
-	// (2) Include TTL, dynamo client succeeds
-	// (1) No TTL, Success
-
 }
 
-func TestPushItem(t *testing.T) {
+func getTestQueryInput() *dynamodb.QueryInput {
+	return &dynamodb.QueryInput{
+		TableName: aws.String("testTable"),
+		KeyConditions: map[string]*dynamodb.Condition{
+			"bucket": {
+				ComparisonOperator: aws.String("EQ"),
+				AttributeValueList: []*dynamodb.AttributeValue{
+					{
+						S: aws.String("testBucket"),
+					},
+				},
+			},
+		},
+		ReturnConsumedCapacity: aws.String(dynamodb.ReturnConsumedCapacityTotal),
+	}
+}
+
+// TODO: candidate for removing
+func testPushItem(t *testing.T) {
 	initGlobalInputs()
 
 	assert := assert.New(t)
@@ -248,7 +261,185 @@ func TestPushItem(t *testing.T) {
 	assert.Equal(expectedConsumedCapacity, actualConsumedCapacity)
 }
 
-func TestGetAllItems(t *testing.T) {
+func getTestFilteredQueryOutput(now time.Time, consumedCapacity *dynamodb.ConsumedCapacity) *dynamodb.QueryOutput {
+	pastExpiration := strconv.Itoa(int(now.Unix() - int64(time.Hour.Seconds())))
+	futureExpiration := strconv.Itoa(int(now.Add(time.Hour).Unix()))
+	bucket := "testBucket"
+
+	return &dynamodb.QueryOutput{
+		ConsumedCapacity: consumedCapacity,
+		Items: []map[string]*dynamodb.AttributeValue{
+			{ // should NOT be included in output (expired item)
+				bucketAttributeKey: {
+					S: aws.String(bucket),
+				},
+				idAttributeKey: {
+					S: aws.String("6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b"),
+				},
+				expirationAttributeKey: {
+					N: aws.String(pastExpiration),
+				},
+			},
+			{ // should be included in output
+				bucketAttributeKey: {
+					S: aws.String(bucket),
+				},
+				idAttributeKey: {
+					S: aws.String("e4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35"),
+				},
+				expirationAttributeKey: {
+					N: aws.String(futureExpiration),
+				},
+			},
+			{ // should be included in output (does not expire)
+				bucketAttributeKey: {
+					S: aws.String(bucket),
+				},
+				idAttributeKey: {
+					S: aws.String("4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce"),
+				},
+			},
+			{ // should NOT be included in output (missing bucket)
+				idAttributeKey: {
+					S: aws.String("5e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce"),
+				},
+			},
+			{ // should NOT be included in output (missing ID)
+				bucketAttributeKey: {
+					S: aws.String(testBucketName),
+				},
+			},
+		},
+	}
+}
+
+func getTestFilteredExpectedItems() map[string]store.OwnableItem {
+	return map[string]store.OwnableItem{
+		"e4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35": {
+			Item: model.Item{
+				ID:  "e4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35",
+				TTL: aws.Int64(3600),
+			},
+		},
+		"4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce": {
+			Item: model.Item{
+				ID: "4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce",
+			},
+		},
+	}
+}
+
+func getTestQueryOutput(now time.Time, consumedCapacity *dynamodb.ConsumedCapacity) *dynamodb.QueryOutput {
+	futureExpiration := strconv.Itoa(int(now.Add(time.Hour).Unix()))
+	bucket := "testBucket"
+	return &dynamodb.QueryOutput{
+		ConsumedCapacity: consumedCapacity,
+		Items: []map[string]*dynamodb.AttributeValue{
+			{ // should be included in output
+				bucketAttributeKey: {
+					S: aws.String(bucket),
+				},
+				idAttributeKey: {
+					S: aws.String("e4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35"),
+				},
+				expirationAttributeKey: {
+					N: aws.String(futureExpiration),
+				},
+			},
+			{ // should be included in output (does not expire)
+				bucketAttributeKey: {
+					S: aws.String(bucket),
+				},
+				idAttributeKey: {
+					S: aws.String("4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce"),
+				},
+			},
+		},
+	}
+}
+
+func getTestExpectedItems() map[string]store.OwnableItem {
+	return map[string]store.OwnableItem{
+		"e4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35": {
+			Item: model.Item{
+				ID:  "e4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35",
+				TTL: aws.Int64(3600),
+			},
+		},
+		"4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce": {
+			Item: model.Item{
+				ID: "4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce",
+			},
+		},
+	}
+}
+
+func getRefTime() time.Time {
+	refTime, err := time.Parse(time.RFC3339, "2021-01-02T15:04:00Z")
+	if err != nil {
+		panic(err)
+	}
+	return refTime
+}
+
+func TestNewGetAll(t *testing.T) {
+	var (
+		dbErr            = errors.New("dynamodb error")
+		consumedCapacity = &dynamodb.ConsumedCapacity{}
+	)
+	nowRef := getRefTime()
+	nowFunc := func() time.Time {
+		return nowRef
+	}
+
+	tcs := []struct {
+		Description              string
+		QueryErr                 error
+		QueryOutput              *dynamodb.QueryOutput
+		ExpectedItems            map[string]store.OwnableItem
+		ExpectedConsumedCapacity *dynamodb.ConsumedCapacity
+		ExpectedErr              error
+	}{
+		{
+			Description:   "Query fails",
+			QueryErr:      dbErr,
+			QueryOutput:   nil,
+			ExpectedErr:   dbErr,
+			ExpectedItems: map[string]store.OwnableItem{},
+		},
+		{
+			Description:              "Expired or bad items",
+			QueryOutput:              getTestFilteredQueryOutput(nowRef, consumedCapacity),
+			ExpectedConsumedCapacity: consumedCapacity,
+			ExpectedItems:            getTestFilteredExpectedItems(),
+		},
+		{
+			Description:              "All good items",
+			QueryOutput:              getTestQueryOutput(nowRef, consumedCapacity),
+			ExpectedConsumedCapacity: consumedCapacity,
+			ExpectedItems:            getTestExpectedItems(),
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.Description, func(t *testing.T) {
+			assert := assert.New(t)
+			m := new(mockClient)
+			svc := executor{
+				c:         m,
+				tableName: "testTable",
+				now:       nowFunc,
+			}
+			m.On("Query", getTestQueryInput()).Return(tc.QueryOutput, tc.QueryErr)
+			items, cc, err := svc.GetAll("testBucket")
+			assert.Equal(tc.ExpectedItems, items)
+			assert.Equal(tc.ExpectedConsumedCapacity, cc)
+			assert.Equal(tc.ExpectedErr, err)
+		})
+	}
+}
+
+// TODO: candidate for deletion
+func testGetAllItems(t *testing.T) {
 	initGlobalInputs()
 	assert := assert.New(t)
 	m := new(mockClient)
@@ -321,7 +512,8 @@ func TestGetAllItems(t *testing.T) {
 	}
 }
 
-func TestDeleteItem(t *testing.T) {
+// TODO: candidate for deletion
+func testDeleteItem(t *testing.T) {
 	assert := assert.New(t)
 	m := new(mockClient)
 	expectedConsumedCapacity := &dynamodb.ConsumedCapacity{
@@ -363,7 +555,8 @@ func TestDeleteItem(t *testing.T) {
 	assert.Equal(expectedConsumedCapacity, actualConsumedCapacity)
 }
 
-func TestGetItem(t *testing.T) {
+// TODO: another candidate for deletion
+func testGetItem(t *testing.T) {
 	initGlobalInputs()
 	now := time.Now().Unix()
 	secondsInHour := int64(time.Hour.Seconds())
