959a1efb49fcc82c9ad1936f3d0b09cde9892030
Joel Unzain <joel_unzain@comcast.com>
2021-05-05 17:48:29 -0700

    add push test

--- store/inmem/InMem_test.go
+++ store/inmem/InMem_test.go
@@ -20,10 +20,96 @@ package inmem
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"github.com/xmidt-org/argus/model"
+	"github.com/xmidt-org/argus/store"
 	"github.com/xmidt-org/argus/store/test"
 )
 
 func TestInMemStore(t *testing.T) {
 	s := ProvideInMem()
 	test.StoreTest(s, 0, t)
+}
+
+type InMemTestSuite struct {
+	suite.Suite
+	BucketName         string
+	ItemID             string
+	Item               store.OwnableItem
+	DataBucketNotFound map[string]map[string]store.OwnableItem
+	DataBucketFound    map[string]map[string]store.OwnableItem
+	DataItemNotFound   map[string]map[string]store.OwnableItem
+	DataItemFound      map[string]map[string]store.OwnableItem
+	DataItemsFound     map[string]map[string]store.OwnableItem
+}
+
+func (s *InMemTestSuite) SetupTest() {
+	s.DataBucketNotFound = map[string]map[string]store.OwnableItem{}
+	s.DataBucketFound = map[string]map[string]store.OwnableItem{
+		s.BucketName: {},
+	}
+	s.DataItemNotFound = map[string]map[string]store.OwnableItem{
+		s.BucketName: {
+			"other-item-id": store.OwnableItem{},
+		},
+	}
+	s.DataItemFound = map[string]map[string]store.OwnableItem{
+		s.BucketName: {
+			s.ItemID: s.Item,
+		},
+	}
+}
+
+func (s *InMemTestSuite) SetupSuite() {
+	s.BucketName = "test-bucket-name"
+	s.ItemID = "test-item-id"
+	s.Item = store.OwnableItem{
+		Owner: "test-owner",
+		Item: model.Item{
+			ID: s.ItemID,
+			Data: map[string]interface{}{
+				"k": "v",
+			},
+		},
+	}
+}
+
+func (s *InMemTestSuite) TestPush() {
+	var expectedData = map[string]map[string]store.OwnableItem{
+		s.BucketName: {
+			s.ItemID: s.Item,
+		},
+	}
+
+	tcs := []struct {
+		Description  string
+		Data         map[string]map[string]store.OwnableItem
+		ExpectedData map[string]map[string]store.OwnableItem
+	}{
+		{
+			Description: "BucketNotFound",
+			Data:        s.DataBucketNotFound,
+		},
+		{
+			Description: "BucketFound",
+			Data:        s.DataBucketFound,
+		},
+	}
+
+	for _, tc := range tcs {
+		s.T().Run(tc.Description, func(t *testing.T) {
+			assert := assert.New(t)
+			storage := InMem{
+				data: tc.Data,
+			}
+			err := storage.Push(model.Key{Bucket: s.BucketName, ID: s.ItemID}, s.Item)
+			assert.Nil(err)
+			assert.EqualValues(expectedData, storage.data)
+		})
+	}
+}
+
+func TestInMem(t *testing.T) {
+	suite.Run(t, new(InMemTestSuite))
 }
