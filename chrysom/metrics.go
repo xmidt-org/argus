0606088b2fbe39f113476d26301a61501c633639
Kristina Pathak <kmspring57@gmail.com>
2021-06-29 20:31:19 -0700

    changed chrysom metrics, added setLogger func

--- chrysom/metrics.go
+++ chrysom/metrics.go
@@ -1,7 +1,26 @@
+/**
+ * Copyright 2021 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package chrysom
 
 import (
-	"github.com/xmidt-org/webpa-common/xmetrics"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/xmidt-org/touchstone"
+	"go.uber.org/fx"
 )
 
 // Names
@@ -21,13 +40,19 @@ const (
 )
 
 // Metrics returns the Metrics relevant to this package
-func Metrics() []xmetrics.Metric {
-	return []xmetrics.Metric{
-		{
-			Name:       PollCounter,
-			Type:       xmetrics.CounterType,
-			Help:       "Counter for the number of polls (and their success/failure outcomes) to fetch new items.",
-			LabelNames: []string{OutcomeLabel},
-		},
-	}
+func ProvideMetrics() fx.Option {
+	return fx.Options(
+		touchstone.CounterVec(
+			prometheus.CounterOpts{
+				Name: PollCounter,
+				Help: "Counter for the number of polls (and their success/failure outcomes) to fetch new items.",
+			},
+			OutcomeLabel,
+		),
+	)
+}
+
+type Measures struct {
+	fx.In
+	Polls *prometheus.CounterVec `name:"chrysom_polls_total"`
 }
