8d150d2a2dffd9c47e01278695c5aee9c5269748
Joel Unzain <joel_unzain@comcast.com>
2021-06-30 13:31:10 -0700

    add doc about implementation

---
+++ store/inmem/doc.go
@@ -0,0 +1,7 @@
+/*
+Package inmem implements the store DAO interface. This implementation is meant
+to help get an instance of Argus up and running quickly without a need to setup
+a dedicated DB. Since the current implementation is not scalable, it is recommended
+for test environments only.
+*/
+package inmem
