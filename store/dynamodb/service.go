7ef42fd3409bec867aef678a748da78b6e932016
maura fortino <maurafortino@gmail.com>
2023-09-22 11:29:45 -0400

    updated license, style, copyright

--- store/dynamodb/service.go
+++ store/dynamodb/service.go
@@ -1,19 +1,5 @@
-/**
- * Copyright 2020 Comcast Cable Communications Management, LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+// SPDX-FileCopyrightText: 2021 Comcast Cable Communications Management, LLC
+// SPDX-License-Identifier: Apache-2.0
 
 package dynamodb
 
@@ -193,7 +179,7 @@ func (d *executor) Delete(key model.Key) (store.OwnableItem, *dynamodb.ConsumedC
 	return d.getOrDelete(key, true)
 }
 
-//TODO: For data >= 1MB, we'll need to handle pagination
+// TODO: For data >= 1MB, we'll need to handle pagination
 func (d *executor) GetAll(bucket string) (map[string]store.OwnableItem, *dynamodb.ConsumedCapacity, error) {
 	result := map[string]store.OwnableItem{}
 	now := strconv.Itoa(int(d.now().Unix()))
