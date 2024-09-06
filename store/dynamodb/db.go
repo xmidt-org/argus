b9cc8342d18591b902692b88828704e7bb719f57
ChaitanyaSingla <charlieksingla@gmail.com>
2024-09-06 11:15:18 +0530

    Remove required tag from Access Key and Secret Key

--- store/dynamodb/db.go
+++ store/dynamodb/db.go
@@ -63,10 +63,10 @@ type Config struct {
 	GetAllLimit int
 
 	// AccessKey is the AWS AccessKey credential.
-	AccessKey string `validate:"required"`
+	AccessKey string
 
 	// SecretKey is the AWS SecretKey credential.
-	SecretKey string `validate:"required"`
+	SecretKey string
 
 	// DisableDualStack indicates whether the connection to the DB should be
 	// dual stack (IPv4 and IPv6).
