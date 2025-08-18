2d27ad59b266b6f9cd45d267765e321cd608c3f6
mpicci200_comcast <marcia_piccione@comcast.com>
2025-08-18 14:31:22 -0400

    fix entrypoint.sh

--- .release/docker/entrypoint.sh
+++ .release/docker/entrypoint.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env sh
 ## SPDX-FileCopyrightText: 2021 Comcast Cable Communications Management, LLC
 ## SPDX-License-Identifier: Apache-2.0
-#!/usr/bin/env sh
+
 set -e
 
 # check arguments for an option that would cause /argus to stop
